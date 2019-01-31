9db1bec16839e57f92914b976db9dfc113804557
root <inboxmotor@gmail.com>
2019-01-31 14:41:23 +0800

    update

--- install_cuda_docker.sh
+++ install_cuda_docker.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Install CUDA
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
